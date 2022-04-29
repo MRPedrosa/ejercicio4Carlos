8ca35e1b3c82e682884f0efd2ef786f8ef7a8a34
MRPedrosa <104232967+MRPedrosa@users.noreply.github.com>
2022-04-29 18:15:44 -0300

    Altura cambio por altura

--- src/datos.ts
+++ src/datos.ts
@@ -3,6 +3,7 @@ let dato = document.forms[0].dato;
 let rotulo = document.getElementById("rotulo");
 
 btnEnv.addEventListener("click", () => {
-  console.log("el dato ingresado es", dato.value);
+  // TU CODIGO VA AQUI, POR EJEMPLO
+  console.log("La altura es", dato.value);
 });
-rotulo.innerHTML = "Ingrese un dato: ";
+rotulo.innerHTML = "Indique la altura de la persona: ";
